924ae66dd2733221cfb87f4dc2dd5e2a5926a3c8
OS-41 <130878336+OS-41@users.noreply.github.com>
2024-06-12 00:26:46 +0900

    more change

--- Vector.js
+++ Vector.js
@@ -1,8 +1,65 @@
 class Vector {
-    constructor(ing) {
-        ing = [0,0];
+    constructor(comp) {
+        comp = [0, 0, 0];
     }
     Add(Vector) {
-        return new Vector(this.ing[0] + Vector[0], this.ing[1] + Vector[1]);
+        if (typeof Vector != Object) {
+            if (Vector.constructor.name == "Vector") {
+                if (Vector.comp.length == this.comp.length) {
+                    return new Vector(this.comp[0] + Vector[0], this.comp[1] + Vector[1], this.comp[2] + Vector[2]);
+                } else {
+                    console.error("there are different length between the two vectors");
+                }
+            } else {
+                console.error("the second argument is not a vector");
+            }
+        } else {
+            console.error("the second argument is not a vector");
+        }
+    }
+    Sub(Vector) {
+        if (typeof Vector != Object) {
+            if (Vector.constructor.name == "Vector") {
+                if (Vector.comp.length == this.comp.length) {
+                    return new Vector(this.comp[0] - Vector[0], this.comp[1] - Vector[1], this.comp[2] - Vector[2]);
+                } else {
+                    console.error("there are different length between the two vectors");
+                }
+            } else {
+                console.error("the second argument is not a vector");
+            }
+        } else {
+            console.error("the second argument is not a vector");
+        }
+    }
+    dot_product(Vector) {
+        if (typeof Vector !== Object) {
+            if (Vector.constructor.name == "Vector") {
+                if (Vector.comp.length == this.comp.length) {
+                    return (this.comp[0] * Vector.comp[0] + this.comp[1] * Vector.comp[1] + this.comp[2] * Vector.comp[2]);
+                } else {
+                    console.error("there are different length between the two vectors");
+                }
+            } else {
+                console.error("the second argument is not a vector");
+            }
+        } else {
+            console.error("the second argument is not a vector");
+        }
+    }
+    cross_product(Vector) {
+        if (typeof Vector !== Object) {
+            if (Vector.constructor.name == "Vector") {
+                if (Vector.comp.length == this.comp.length) {
+                    return new Vector(this.comp[1] * Vector.comp[2] - this.comp[2] * Vector.comp[1], this.comp[2] * Vector.comp[1] - this.comp[1] * Vector.comp[2],this.comp[1] * Vector.comp[2] - this.comp[2] * Vector.comp[1]);
+                } else {
+                    console.error("there are different length between the two vectors");
+                }
+            } else {
+                console.error("the second argument is not a vector");
+            }
+        } else {
+            console.error("the second argument is not a vector");
+        }
     }
 }
